f76ad9e9bf86ea54588629c640f397fb96e44f54
Cader <mcader@hasnatech.com>
2019-07-11 10:23:28 +0530

    Home page layout completed for review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { Home1Component } from './home1/home1.component';
+import { ProcessComponent } from './process/process.component';
 
 const routes: Routes = [
   {
     path:"",
     component: Home1Component
+  },
+  {
+    path:"process/:id",
+    component:ProcessComponent
+
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Home1Component } from './home1/home1.component';
+import { ProcessComponent } from './process/process.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Home1Component
+    Home1Component,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
